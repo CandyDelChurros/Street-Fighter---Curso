7a1639ec0876ec28d26fa0890166208de3cecb3b
CandyDelChurros <cayo.veiga54@gmail.com>
2024-03-12 19:55:38 -0300

    arrumando as div e o js

--- script/script.js
+++ script/script.js
@@ -1,35 +1,27 @@
-function aki(){
-    const menu = ["aki-status", "aki-status-mid", "aki-status-bot"]
+function aki() {
+    const menu = ["aki-status", "aki-status-mid", "aki-status-bot"];
     menu.forEach((aparece) => {
-    document.getElementById(aparece).style.visibility = 'visible';
-    })
+        document.getElementById(aparece).style.visibility = 'visible';
+    });
 
-    document.getElementById("aki").onmouseenter = function(){
-        this.style.background = 'url(../img/bg_aki.jpg)';
-    }
-    document.getElementById("aki").onmouseleave = function(){
-        this.style.background = 'url(../img/bg_aki.jpg)';
-    }
+    const bg = ["aki_img_person", "aki_p_hover"];
+    bg.forEach((some) => {
+    Array.from(document.getElementsByClassName(some)).forEach((element) => { element.style.filter = 'blur(8px)'; });
+    });
 }
-
 function desaparecer() {
-    var aki = document.getElementById('aki');
     var akiStatus = document.getElementById('aki-status');
     var akiStatusMid = document.getElementById('aki-status-mid');
     var akiStatusBot = document.getElementById('aki-status-bot');
 
-    aki.style.background = 'url(../img/aki_houver.png) center/68rem';
     akiStatus.style.visibility = 'hidden';
     akiStatusMid.style.visibility = 'hidden';
     akiStatusBot.style.visibility = 'hidden';
 
-    aki.onmouseenter = function() {
-        this.style.background = 'url(../img/aki.png) center/68rem';
-    };
-
-    aki.onmouseleave = function() {
-        this.style.background = 'url(../img/aki_houver.png) center/68rem';
-    };
+    const bg = ["aki_img_person", "aki_p_hover"];
+    bg.forEach((some) => {
+    Array.from(document.getElementsByClassName(some)).forEach((element) => { element.style.filter = 'none'; });
+    });
 }
 
 
