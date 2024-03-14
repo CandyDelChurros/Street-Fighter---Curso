69d2d057142ffdef8fc6eb99284b302f79c48892
CandyDelChurros <cayo.veiga54@gmail.com>
2024-03-14 15:18:22 -0300

    javascript funcionando

--- script/script.js
+++ script/script.js
@@ -45,13 +45,6 @@ function juri() {
     });
 }
 
-
-
-
-
-
-
-
 function aki_desper() {
     var akiStatus = document.getElementById('aki-status-top');
     var akiStatusMid = document.getElementById('aki-status-mid');
@@ -71,39 +64,40 @@ function cammy_desper() {
     var cammyStatusMid = document.getElementById('cammy-status-mid');
     var cammyStatusBot = document.getElementById('cammy-status-bot');
 
-    akiStatus.style.visibility = 'hidden';
-    akiStatusMid.style.visibility = 'hidden';
-    akiStatusBot.style.visibility = 'hidden';
+    cammyStatus.style.visibility = 'hidden';
+    cammyStatusMid.style.visibility = 'hidden';
+    cammyStatusBot.style.visibility = 'hidden';
 
-    const bg = ["aki_img_person", "aki_p_hover"];
+    const bg = ["cammy_img_person", "cammy_p_hover"];
     bg.forEach((some) => {
     Array.from(document.getElementsByClassName(some)).forEach((element) => { element.style.filter = 'none'; });
     });
 }
-function aki_desper() {
-    var akiStatus = document.getElementById('aki-status-top');
-    var akiStatusMid = document.getElementById('aki-status-mid');
-    var akiStatusBot = document.getElementById('aki-status-bot');
 
-    akiStatus.style.visibility = 'hidden';
-    akiStatusMid.style.visibility = 'hidden';
-    akiStatusBot.style.visibility = 'hidden';
+function marissa_desper() {
+    var marissaStatus = document.getElementById('marissa-status-top');
+    var marissaStatusMid = document.getElementById('marissa-status-mid');
+    var marissaStatusBot = document.getElementById('marissa-status-bot');
 
-    const bg = ["aki_img_person", "aki_p_hover"];
+    marissaStatus.style.visibility = 'hidden';
+    marissaStatusMid.style.visibility = 'hidden';
+    marissaStatusBot.style.visibility = 'hidden';
+
+    const bg = ["marissa_img_person", "marissa_p_hover"];
     bg.forEach((some) => {
     Array.from(document.getElementsByClassName(some)).forEach((element) => { element.style.filter = 'none'; });
     });
 }
-function aki_desper() {
-    var akiStatus = document.getElementById('aki-status-top');
-    var akiStatusMid = document.getElementById('aki-status-mid');
-    var akiStatusBot = document.getElementById('aki-status-bot');
+function juri_desper() {
+    var juriStatus = document.getElementById('juri-status-top');
+    var juriStatusMid = document.getElementById('juri-status-mid');
+    var juriStatusBot = document.getElementById('juri-status-bot');
 
-    akiStatus.style.visibility = 'hidden';
-    akiStatusMid.style.visibility = 'hidden';
-    akiStatusBot.style.visibility = 'hidden';
+    juriStatus.style.visibility = 'hidden';
+    juriStatusMid.style.visibility = 'hidden';
+    juriStatusBot.style.visibility = 'hidden';
 
-    const bg = ["aki_img_person", "aki_p_hover"];
+    const bg = ["juri_img_person", "juri_p_hover"];
     bg.forEach((some) => {
     Array.from(document.getElementsByClassName(some)).forEach((element) => { element.style.filter = 'none'; });
     });
